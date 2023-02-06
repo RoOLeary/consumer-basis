417d2a31f5300d8812490e964a8e5712294187b8
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2023-02-06 14:29:02 +0100

    tweak filter

--- src/pages/jobs/index.jsx
+++ src/pages/jobs/index.jsx
@@ -15,7 +15,7 @@ import { JoinUs } from '@/components/JoinUs'
 import { Video } from '@/components/Video'
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import { jobs } from './../../lib/utils';
 
@@ -25,6 +25,25 @@ export default function Jobs() {
   const control = useAnimation()
   const [ref, inView] = useInView({ triggerOnce: false});
 
+  const [query, setQuery] = useState('');
+  const searchForm = useRef(null);
+  
+  const searchFilter = (array) => {
+    return array.filter(  
+       // eslint-disable-next-line no-console
+        (el) => el.title.toLowerCase().includes(query)
+    )
+  }
+  
+  //Applying our search filter function to our array of countries recieved from the API
+  const filtered = searchFilter(jobs);
+  // eslint-disable-next-line no-console
+  //Handling the input on our search bar
+  const handleChange = (e) => {
+    // eslint-disable-next-line no-console
+    setQuery(e.target.value)
+    console.log(e.target.value)
+  }
 
   return (
     <>
@@ -156,14 +175,72 @@ export default function Jobs() {
                       <form className="hidden lg:block">
                       
                         <div className="border-b border-gray-200 py-6">
-
+                          {/*  Text Filter  */}
                           <h3 className="-my-3 flow-root mb-4">
                             <span className="font-medium text-gray-900">Filter</span>
                           </h3>
                           <div className="col-span-6 sm:col-span-3">
-                            <input type="text" name="search-term" id="search-term" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-sm" placeholder="Wat wil je doen?" />
+                            <input onChange={handleChange} type="text" name="search-term" id="search-term" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-sm" placeholder="Wat wil je doen?" />
                           </div>
                         </div>
+                       
+                        <div className="border-b border-gray-200 py-6">
+
+                          {/* Department */}
+                          <h3 className="-my-3 flow-root">
+
+                            <button type="button" className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500" aria-controls="filter-section-0" aria-expanded="false">
+                              <span className="font-medium text-gray-900">Department</span>
+                            </button>
+                          </h3>
+
+                          <div className="pt-6" id="filter-section-0">
+                            <div className="space-y-4">
+                              <div className="flex items-center">
+                                <input id="filter-color-0" name="color[]" value="white" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-0" className="ml-3 text-sm text-gray-600">Commercial</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-1" name="color[]" value="beige" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-1" className="ml-3 text-sm text-gray-600">Creative</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-2" name="color[]" value="blue" type="checkbox" defaultChecked className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-2" className="ml-3 text-sm text-gray-600">Technology</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-3" name="color[]" value="brown" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-3" className="ml-3 text-sm text-gray-600">Logistics</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-0" name="color[]" value="white" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-0" className="ml-3 text-sm text-gray-600">People</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-1" name="color[]" value="beige" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-1" className="ml-3 text-sm text-gray-600">Facilities</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-2" name="color[]" value="blue" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-2" className="ml-3 text-sm text-gray-600">Data Analytics</label>
+                              </div>
+
+                              <div className="flex items-center">
+                                <input id="filter-color-3" name="color[]" value="brown" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <label htmlFor="filter-color-3" className="ml-3 text-sm text-gray-600">Finance</label>
+                              </div>
+
+                              
+                            </div>
+                          </div>
+                        </div>
+                        {/* Availabity */}
                         <div className="border-b border-gray-200 py-6">
 
                       
@@ -177,7 +254,7 @@ export default function Jobs() {
                           <div className="pt-6" id="filter-section-0">
                             <div className="space-y-4">
                               <div className="flex items-center">
-                                <input id="filter-color-0" name="color[]" value="white" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <input id="filter-color-0" name="color[]" value="white" type="checkbox" defaultChecked className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
                                 <label htmlFor="filter-color-0" className="ml-3 text-sm text-gray-600">Full time</label>
                               </div>
 
@@ -187,7 +264,7 @@ export default function Jobs() {
                               </div>
 
                               <div className="flex items-center">
-                                <input id="filter-color-2" name="color[]" value="blue" type="checkbox" defaultChecked className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <input id="filter-color-2" name="color[]" value="blue" type="checkbox"  className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
                                 <label htmlFor="filter-color-2" className="ml-3 text-sm text-gray-600">Weekend</label>
                               </div>
 
@@ -200,7 +277,7 @@ export default function Jobs() {
                             </div>
                           </div>
                         </div>
-
+                        {/* Location */}
                         <div className="border-b border-gray-200 py-6">
                           <h3 className="-my-3 flow-root">
                           
@@ -212,17 +289,17 @@ export default function Jobs() {
                           <div className="pt-6" id="filter-section-1">
                             <div className="space-y-4">
                               <div className="flex items-center">
-                                <input id="filter-category-0" name="category[]" value="new-arrivals" type="checkbox" defaultChecked className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <input id="filter-category-0" name="category[]" value="nl" type="checkbox" defaultChecked className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
                                 <label htmlFor="filter-category-0" className="ml-3 text-sm text-gray-600">The Netherlands</label>
                               </div>
 
                               <div className="flex items-center">
-                                <input id="filter-category-1" name="category[]" value="sale" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <input id="filter-category-1" name="category[]" value="fr" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
                                 <label htmlFor="filter-category-1" className="ml-3 text-sm text-gray-600">France</label>
                               </div>
 
                               <div className="flex items-center">
-                                <input id="filter-category-2" name="category[]" value="travel" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
+                                <input id="filter-category-2" name="category[]" value="de" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
                                 <label htmlFor="filter-category-2" className="ml-3 text-sm text-gray-600">Germany</label>
                               </div>
 
@@ -231,52 +308,6 @@ export default function Jobs() {
                           </div>
                         </div>
 
-                        {/* <div className="border-b border-gray-200 py-6">
-                          <h3 className="-my-3 flow-root">
-                            <button type="button" className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500" aria-controls="filter-section-2" aria-expanded="false">
-                              <span className="font-medium text-gray-900">Size</span>
-                              <span className="ml-6 flex items-center">
-                                
-                                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                  <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                                </svg>
-                                
-                                <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                  <path fillRule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clipRule="evenodd" />
-                                </svg>
-                              </span>
-                            </button>
-                          </h3>
-                        
-                          <div className="pt-6" id="filter-section-2">
-                            <div className="space-y-4">
-                              <div className="flex items-center">
-                                <input id="filter-size-0" name="size[]" value="2l" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
-                                <label htmlFor="filter-size-0" className="ml-3 text-sm text-gray-600">2L</label>
-                              </div>
-                              <div className="flex items-center">
-                                <input id="filter-size-1" name="size[]" value="6l" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
-                                <label htmlFor="filter-size-1" className="ml-3 text-sm text-gray-600">6L</label>
-                              </div>
-                              <div className="flex items-center">
-                                <input id="filter-size-2" name="size[]" value="12l" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
-                                <label htmlFor="filter-size-2" className="ml-3 text-sm text-gray-600">12L</label>
-                              </div>
-                              <div className="flex items-center">
-                                <input id="filter-size-3" name="size[]" value="18l" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
-                                <label htmlFor="filter-size-3" className="ml-3 text-sm text-gray-600">18L</label>
-                              </div>
-                              <div className="flex items-center">
-                                <input id="filter-size-4" name="size[]" value="20l" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
-                                <label htmlFor="filter-size-4" className="ml-3 text-sm text-gray-600">20L</label>
-                              </div>
-                              <div className="flex items-center">
-                                <input id="filter-size-5" name="size[]" value="40l" type="checkbox" defaultChecked className="h-4 w-4 rounded border-gray-300 text-red-600 focus:ring-red-500" />
-                                <label htmlFor="filter-size-5" className="ml-3 text-sm text-gray-600">40L</label>
-                              </div>
-                            </div>
-                          </div>
-                        </div> */}
                       </form>
 
                       <div className="lg:col-span-3">
@@ -558,7 +589,7 @@ export default function Jobs() {
               </div>
             </div>
       </section>
-      <PrimaryFeatures />
+      
       <JobTestimonial />
       <Video /> 
       <TextVisual />
